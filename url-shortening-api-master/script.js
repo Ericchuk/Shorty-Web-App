147d5cc8daea8d965c041549fb7da7bd503fc8d7
Ericchuks <you@example.com>
2022-07-05 09:36:41 +0100

    new

---
+++ url-shortening-api-master/script.js
@@ -0,0 +1,106 @@
+let hamburger = document.querySelector(".hamburger");
+let close = document.querySelector(".close");
+let nav = document.querySelector("header nav");
+let input = document.querySelector("input");
+let shortIt = document.querySelector(".callToAction");
+let advance = document.querySelector(".advance");
+let button = document.querySelector(".btn")
+
+
+function openMenu(){
+    if(nav.style.display == "flex"){
+        nav.style.display = "none";
+        hamburger.style.display = "block";
+        close.style.display = "none";
+        console.log("ddd")
+    }else{
+        nav.style.display = "flex";
+        hamburger.style.display = "none";
+        close.style.display = "block";
+        console.log("999")
+    }
+}
+
+hamburger.addEventListener("click", openMenu)
+close.addEventListener("click", openMenu)
+
+let grow = document.querySelector(".grow");
+let links = JSON.parse(localStorage.getItem("links")) || [];
+function checkInput(){
+    let url  = input.value;
+    
+    let apiAddress = "https://api.shrtco.de/v2/shorten";
+    apiAddress = `https://api.shrtco.de/v2/shorten?url=${url}`
+    let regex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    let address = `${apiAddress}?${input.value}`;
+    
+    if(input.value != "" && input.value.match(regex)){
+        // console.log(address);
+        async function getApi(){
+            try{
+               let apiData = await fetch(apiAddress);
+               let response = await apiData.json();links.push([response.result.full_short_link, response.result.original_link]);
+               for(let i = 0; i < links.length; i++){
+                // console.log(response);
+                let div = document.createElement("div");
+                div.className = "result"
+                let p = document.createElement("p");
+                p.className = "fromJs";
+                p.textContent = `${response.result.original_link}`
+                let ul = document.createElement("ul");
+                // ul.appendChild(li)
+                let hr  = document.createElement("hr");
+                hr.className = "hr"
+                // ul.appendChild(button)
+                let li = document.createElement("li");
+                li.textContent = `${response.result.full_short_link}`
+                let button = document.createElement("button");
+                button.textContent = "Copy"
+                button.className = "btn";
+                div.appendChild(p)
+                div.appendChild(hr)
+                div.appendChild(ul);
+                ul.appendChild(li);
+                ul.appendChild(button)
+                grow.appendChild(div);
+                return
+               }
+
+               
+            }
+            catch{
+                console.error();
+            }
+            
+            
+        }
+        // console.log(advance,res.result.original_link)
+        getApi();
+    }else{
+        console.log("lll");
+    }
+}
+
+shortIt.addEventListener("click", checkInput)
+checkInput();
+
+
+// // XMLHttpRequest
+// let xmlhttp = new XMLHttpRequest();
+
+// xmlhttp.onload = function(){
+//     // if(this.readyState == 4 && this.status == 200){
+//         console.log(xmlhttp.responseText);
+//        let  response = JSON.parse(xmlhttp.responseText);
+//     //    let title = response.title;
+//     //    let divValue1 = document.querySelector(".json-overlay");
+//       console.log("ddd")
+//         for(let i = 0; i < response.length; i++){}
+// }
+// // }
+
+// let url = input.value;  
+// let apiAddress = "https://api.shrtco.de/v2/shorten";
+//     apiAddress = `https://api.shrtco.de/v2/shorten?url=${url}`
+// xmlhttp.open("GET", apiAddress);
+// xmlhttp.send();
